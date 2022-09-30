4ac0825a27fd134d13c7d0fde2c6dd3befc5efb3
Carlo van Kessel <carlovankessel@yahoo.nl>
2022-09-30 15:42:54 +0200

    New query

--- LambdaFunctions/getRetailersByBrand/retailers_by_brand_name_handler.py
+++ LambdaFunctions/getRetailersByBrand/retailers_by_brand_name_handler.py
@@ -12,11 +12,26 @@ conn = psycopg2.connect(
 )
 
 def lambda_handler(event, context):
-    curr = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
-    curr.execute("select * from ")
-    results = curr.fetchall()
-    json_result = json.dumps(results)
-    print(json_result)
-    return json_result
+    # curr1 = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+    # curr1.execute("SELECT id FROM brand WHERE brand.name = " + parameter)
+    # results1 = curr1.fetchall()
+    # json_result1 = json.dumps(results1)
+
+    # curr2 = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+    # curr2.execute("SELECT * FROM retailer JOIN brand_retailer ON brand_retailer.brand_id = " + json_result1)
+    # results2 = curr2.fetchall()
+    # json_result2 = json.dumps(results2)
+    # print(json_result2)
+    # return json_result2
+
+    # curr3 = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+    # curr3.execute("    select retailer.* from retailer INNER JOIN brand_retailer ON retailer.id=brand_retailer.retailer_id INNER JOIN brand ON brand_retailer.brand_id=brand.id WHERE brand.name=" + parameter)
+    # results3 = curr3.fetchall()
+    # json_result3 = json.dumps(results3)
+    # print(json_result3)
+
+    return event['pathParameters']['param1']
+
+
 
 #lambda_handler()
