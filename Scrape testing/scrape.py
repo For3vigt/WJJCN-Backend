ee35f20617ef8f435faa1223f3eaede116c2256d
JarnoStudent <481737@student.fontys.nl>
2022-10-06 13:47:50 +0200

    Can get to item specifications

--- Scrape testing/scrape.py
+++ Scrape testing/scrape.py
@@ -20,39 +20,48 @@ chrome_options.add_argument('start-maximized')
 d = webdriver.Chrome(service=Service(
     ChromeDriverManager
     ().install()), chrome_options=chrome_options)
-product = 'Red Bull Energy Drink'
+product = 'Red Bull Energy Drink 4-pack'
 
 d.get('https://www.jumbo.com/')
 
-try :
+try:
     element = WebDriverWait(d, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Weigeren')]"))
+        EC.presence_of_element_located(
+            (By.XPATH, "//*[contains(text(), 'Weigeren')]"))
     )
 finally:
     if element:
         d.find_element(By.XPATH, "//*[contains(text(), 'Weigeren')]").click()
-    
-try :
+
+try:
     element = WebDriverWait(d, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//input[@placeholder='Waar ben je naar op zoek?']"))
+        EC.presence_of_element_located(
+            (By.XPATH, "//input[@placeholder='Waar ben je naar op zoek?']"))
     )
 finally:
     if element:
-        e = d.find_element(By.XPATH, "//input[@placeholder='Waar ben je naar op zoek?']")
+        e = d.find_element(
+            By.XPATH, "//input[@placeholder='Waar ben je naar op zoek?']")
         e = e.send_keys(product + '\n')
-try :
+try:
     element = WebDriverWait(d, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//*[text()[contains(.,"+ "'"+ product+ "')]]"))
+        EC.presence_of_element_located(
+            (By.XPATH, "//*[normalize-space(text()) = '" + product + "']"))
     )
-finally:
+except:
+    try:
+        elementContains = WebDriverWait(d, 10).until(
+            EC.presence_of_element_located(
+                (By.XPATH, "//a[text()[contains(.,'" + product + "')]]"))
+        )
+    except:
+        print("Something went wrong!")
+
+    else:
+        if elementContains:
+            hoer = d.find_element(
+                By.XPATH,  "//a[text()[contains(.,'" + product + "')]]").click()
+else:
     if element:
-        e = d.find_element(By.XPATH, "//*[text()[contains(.,"+ "'"+ product+ "')]]").click()
-        
-
-
-
-
-#e = d.find_element(By.XPATH, "/html/body/div[1]/div/div/div[1]/header/div[2]/div[2]/div[1]/div[1]/form/div/input")
-#e = d.find_element(By.XPATH, "//*[contains (text(), 'Waar ben je')]")
-
-
+        e = d.find_element(
+            By.XPATH,  "//*[normalize-space(text()) = '" + product + "']").click()
