c2c28c8e3d633284246775d4232128e6c4d0d3e8
JarnoStudent <481737@student.fontys.nl>
2022-10-07 09:20:18 +0200

    Added some comments

--- Scrape testing/scrape.py
+++ Scrape testing/scrape.py
@@ -11,19 +11,21 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
 from selenium.webdriver.chrome.options import Options
-
+# Chrome options for browser its going to open
 chrome_options = Options()
-
 chrome_options.add_experimental_option("detach", True)
 chrome_options.add_argument('start-maximized')
 
+# Run driver using Chrome
 d = webdriver.Chrome(service=Service(
     ChromeDriverManager
     ().install()), chrome_options=chrome_options)
-product = 'Red Bull Energy Drink 4-pack'
 
+# Product that will be searched and link of the website it will use
+product = 'Red Bull Energy Drink 4-pack'
 d.get('https://www.jumbo.com/')
 
+# Try to get past cookies
 try:
     element = WebDriverWait(d, 1).until(
         EC.presence_of_element_located(
@@ -33,6 +35,7 @@ finally:
     if element:
         d.find_element(By.XPATH, "//*[contains(text(), 'Weigeren')]").click()
 
+# Try to find search bar
 try:
     element = WebDriverWait(d, 1).until(
         EC.presence_of_element_located(
@@ -43,6 +46,8 @@ finally:
         e = d.find_element(
             By.XPATH, "//input[@placeholder='Waar ben je naar op zoek?']")
         e = e.send_keys(product + '\n')
+
+# Try to click the right product
 try:
     element = WebDriverWait(d, 1).until(
         EC.presence_of_element_located(
@@ -55,7 +60,7 @@ except:
                 (By.XPATH, "//a[text()[contains(.,'" + product + "')]]"))
         )
     except:
-        print("Something went wrong!")
+        print("Could not find element!")
 
     else:
         if elementContains:
@@ -66,11 +71,11 @@ else:
         e = d.find_element(
             By.XPATH,  "//*[normalize-space(text()) = '" + product + "']").click()
 
-
+# Try to find excel information on the product page
 try:
     e = d.find_element(
         By.XPATH,  "//*[text()[contains(.,'" + product + "')]]")
 except:
-    print("Something went terribly wrong!")
+    print("Could not find element!")
 else:
     print("Added score to database!")
