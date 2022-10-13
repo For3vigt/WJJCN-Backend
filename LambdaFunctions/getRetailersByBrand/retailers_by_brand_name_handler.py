bc3d610a2f4c07f8cfcd2436cdfe91803ed8cc24
Carlo van Kessel <carlovankessel@yahoo.nl>
2022-10-13 13:14:16 +0200

    Added new get retailers by brand name function with pymongo module

--- LambdaFunctions/getRetailersByBrand/retailers_by_brand_name_handler.py
+++ LambdaFunctions/getRetailersByBrand/retailers_by_brand_name_handler.py
@@ -1,37 +1,17 @@
 from http.client import ImproperConnectionState
-import psycopg2
-import psycopg2.extras
 import json
 import os
-
-conn = psycopg2.connect(
-    host = os.environ.get('db_host'),
-    database = os.environ.get('db'),
-    user = os.environ.get('db_user'),
-    password = os.environ.get('db_pass')
-)
+import pymongo
+import bson.json_util as json_util
+myclient = pymongo.MongoClient(os.environ.get('db_host'))
+mydb = myclient[os.environ.get("db")]
+mycol = mydb["brand_retailer_products"]
 
 def lambda_handler(event, context):
-    # curr1 = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
-    # curr1.execute("SELECT id FROM brand WHERE brand.name = " + parameter)
-    # results1 = curr1.fetchall()
-    # json_result1 = json.dumps(results1)
-
-    # curr2 = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
-    # curr2.execute("SELECT * FROM retailer JOIN brand_retailer ON brand_retailer.brand_id = " + json_result1)
-    # results2 = curr2.fetchall()
-    # json_result2 = json.dumps(results2)
-    # print(json_result2)
-    # return json_result2
-
-    # curr3 = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
-    # curr3.execute("    select retailer.* from retailer INNER JOIN brand_retailer ON retailer.id=brand_retailer.retailer_id INNER JOIN brand ON brand_retailer.brand_id=brand.id WHERE brand.name=" + parameter)
-    # results3 = curr3.fetchall()
-    # json_result3 = json.dumps(results3)
-    # print(json_result3)
-
-    return event['pathParameters']['param1']
-
-
-
+    brandName = '{}'.format(event['name'])
+    mysql = { "brand": brandName }
+    mydoc = mycol.find(mysql)
+    json_result = json.loads(json_util.dumps(mydoc))
+    
+    return(json_result)
 #lambda_handler()
