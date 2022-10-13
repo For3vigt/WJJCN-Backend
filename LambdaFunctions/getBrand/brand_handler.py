1546a4d26e2d40a11832ac769bfef7f2eb1083e5
Carlo van Kessel <carlovankessel@yahoo.nl>
2022-10-13 11:35:13 +0200

    Added new get brand function with pymongo module

--- LambdaFunctions/getBrand/brand_handler.py
+++ LambdaFunctions/getBrand/brand_handler.py
@@ -1,22 +1,15 @@
 from http.client import ImproperConnectionState
-import psycopg2
-import psycopg2.extras
 import json
 import os
-
-conn = psycopg2.connect(
-    host = os.environ.get('db_host'),
-    database = os.environ.get('db'),
-    user = os.environ.get('db_user'),
-    password = os.environ.get('db_pass')
-)
+import pymongo
+import bson.json_util as json_util
+myclient = pymongo.MongoClient(os.environ.get('db_host'))
+mydb = myclient[os.environ.get("db")]
+mycol = mydb["brand"]
 
 def lambda_handler(event, context):
-    curr = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
-    curr.execute("select * from brand")
-    results = curr.fetchall()
-    json_result = json.dumps(results)
-    print(json_result)
-    return json_result
-
+    mydoc = mycol.find()
+    json_result = json.loads(json_util.dumps(mydoc))
+    
+    return(json_result)
 #lambda_handler()
